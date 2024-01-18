090587167bcfca551abea9a818ec78639403860d
daviddieppois <david.dieppois@smilecdr.com>
2024-01-18 18:13:29 -0500

    Updated test to specifically test for cqframework.cql extension being active

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,41 +5,16 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
-suite('CQL Code Suggestions', () => {
-  test('Should provide code suggestions after typing "log" in a .cql file', async () => {
-    vscode.window.showInformationMessage('Start vscode-cql extension tests.');
-    // Open a .cql file
-    const doc = await vscode.workspace.openTextDocument({
-      language: 'cql', // Specify the language ID for .cql files
-      content: '', // Start with an empty document
-    });
-    await vscode.window.showTextDocument(doc);
+suite('Extension Activation Test', () => {
+  test('Extension should be present', () => {
+    assert.ok(vscode.extensions.getExtension('cqframework.cql'));
+  });
 
-    // Simulate typing "log" into the document
-    await vscode.commands.executeCommand('type', { text: 'log' });
-
-    // Trigger IntelliSense (e.g., by typing a space after "log")
-    const suggestions = await vscode.commands.executeCommand(
-      'vscode.executeCompletionItemProvider',
-      doc.uri,
-      new vscode.Position(0, 3) // Position cursor after "log"
-    );
-
-    // Assert that suggestions have been provided
-    assert.ok(suggestions);
-
-    // @ts-ignore
-    assert.ok(suggestions.items.length > 0);
-
-    // List all suggestions and their details
-    // @ts-ignore
-    suggestions.items.forEach((item, index) => {
-      console.log(`Suggestion ${index + 1}:`);
-      console.log(`Label: ${item.label}`);
-      console.log(`Kind: ${item.kind}`);
-      console.log(`Insert Text: ${item.insertText}`);
-      console.log(`Detail: ${item.detail}`);
-      console.log('---');
-    });
+  test('Extension should be active', async () => {
+    const extension = vscode.extensions.getExtension('cqframework.cql');
+    if (!extension?.isActive) {
+      await extension?.activate();
+    }
+    assert.ok(extension && extension.isActive);
   });
 });
