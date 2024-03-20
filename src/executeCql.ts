3c941f4a8d7a99661e6f20c1762189f1ae8674a6
Jonathan Percival <jonathan.i.percival@gmail.com>
2024-03-20 11:54:08 -0600

    Clean up CQL execution code, try to universally use URIs

--- src/executeCql.ts
+++ src/executeCql.ts
@@ -1,47 +1,100 @@
-import { window, workspace, commands, Uri, TextEditor, Position } from "vscode";
+import { glob } from "glob";
+import { Position, TextEditor, Uri, commands, window, workspace } from "vscode";
+import { Utils } from "vscode-uri";
 import { Commands } from "./commands";
 
-import * as fse from "fs-extra";
-import * as path from "path";
 import * as fs from "fs";
+import * as fse from "fs-extra";
+import path from "path";
 
 // NOTE: This is not the intended future state of executing CQL.
 // There's a CQL debug server under development that will replace this.
 export async function executeCQLFile(uri: Uri): Promise<void> {
-	const libraryPath: string = uri.fsPath;
-	if (!fs.existsSync(libraryPath)) {
+	if (!fs.existsSync(uri.fsPath)) {
 		window.showInformationMessage(
 			"No library content found. Please save before executing."
 		);
 		return;
 	}
 
-	const libraryPathName = path.basename(libraryPath, ".cql");
-
-	// todo: replace with library-uri when it's ready
-	const libraryDirectory = path.dirname(libraryPath);
-	const libraryName = libraryPathName.split("-")[0];
-
-	const projectPath = workspace.getWorkspaceFolder(uri)!.uri.fsPath;
+	const libraryDirectory = Utils.dirname(uri);
+	const libraryName = Utils.basename(uri).replace(".cql", "").split("-")[0];
+	const projectPath = workspace.getWorkspaceFolder(uri)!.uri;
 
 	// todo: make this a setting
-	const terminologyPath = path.join(
+	let terminologyPath : Uri = Utils.resolvePath(
 		projectPath,
 		"input",
 		"vocabulary",
 		"valueset"
 	);
 
-	// todo: get this working (currently errors with: Index 0 out of bounds for length 0)
-	// const measurementPeriod = 'Interval[@2019-01-01T00:00:00.0, @2020-01-01T00:00:00.0)';
-	const modelType = "FHIR";
-	const contextType = "Patient";
-	let fhirVersion = "R4";
-	const optionsPath = path.join(libraryDirectory, "cql-options.json");
-	const measurementPeriod = "";
-	const testPath = path.join(projectPath, "input", "tests");
-	const resultPath = path.join(testPath, "results");
+	let fhirVersion = getFhirVersion();
+	if (!fhirVersion) {
+		fhirVersion = "R4";
+		window.showInformationMessage(
+			"Unable to determine version of FHIR used. Defaulting to R4."
+		);
+	}
 
+	const optionsPath = Utils.resolvePath(libraryDirectory, "cql-options.json");
+	const measurementPeriod = "";
+	const testPath = Utils.resolvePath(projectPath, "input", "tests");
+	const resultPath = Utils.resolvePath(testPath, "results");
+	const outputPath = Utils.resolvePath(resultPath, `${libraryName}.txt`);
+
+	fse.ensureFileSync(outputPath.fsPath);
+
+	const textDocument = await workspace.openTextDocument(outputPath);
+	const textEditor = await window.showTextDocument(textDocument);
+
+	var testCasesArgs : string[] = []
+	var testPaths = getTestPaths(testPath, libraryName);
+
+	// We didn't find any test cases, so we'll just execute an empty one
+	if (testPaths.length === 0) {
+		testPaths.push({ name : null, path : null});
+	}
+	
+	for (var p of testPaths) {
+		testCasesArgs.push(...getExecArgs(
+			libraryDirectory,
+			libraryName,
+			p.path,
+			terminologyPath, 
+			p.name,
+			measurementPeriod
+		));
+	}
+
+	const cqlMessage = `CQL: ${libraryDirectory.fsPath}`;
+	const terminologyMessage =
+		fs.existsSync(terminologyPath.fsPath)
+			? `Terminology: ${terminologyPath.fsPath}`
+			: `No terminology found at ${terminologyPath.fsPath}. Evaluation may fail if terminology is required.`;
+
+	let testMessage = "";
+	if (testPaths.length == 1 && testPaths[0].name === null) {
+		testMessage = `No data found at ${testPath.fsPath}. Evaluation may fail if data is required.`;
+	}
+	else {
+		testMessage = `Test cases:\n`
+		for (var p of testPaths) {
+			testMessage += `${p.name} - ${p.path?.fsPath}\n`;
+		}
+	}
+
+	await insertLineAtEnd(textEditor, `${cqlMessage}`);
+	await insertLineAtEnd(textEditor, `${terminologyMessage}`);
+	await insertLineAtEnd(textEditor, `${testMessage}`);
+
+
+	let operationArgs = getCqlCommandArgs(fhirVersion, optionsPath);
+	operationArgs.push(...testCasesArgs);
+	await executeCQL(textEditor, operationArgs);
+}
+
+function getFhirVersion(): string | null {
 	const fhirVersionRegex = /using (FHIR|"FHIR") version '(\d(.|\d)*)'/;
 	const matches = window
 		.activeTextEditor!.document.getText()
@@ -49,138 +102,54 @@ export async function executeCQLFile(uri: Uri): Promise<void> {
 	if (matches && matches.length > 2) {
 		const version = matches[2];
 		if (version.startsWith("2")) {
-			fhirVersion = "DSTU2";
+			return "DSTU2";
 		} else if (version.startsWith("3")) {
-			fhirVersion = "DSTU3";
+			return "DSTU3";
 		} else if (version.startsWith("4")) {
-			fhirVersion = "R4";
+			return  "R4";
 		} else if (version.startsWith("5")) {
-			fhirVersion = "R5";
+			return"R5";
 		}
-	} else {
-		fhirVersion = "R4";
-		window.showInformationMessage(
-			"Unable to determine version of FHIR used. Defaulting to R4."
-		);
 	}
-
-	// Recursively creates directory
-	// mkDirByPathSync(resultPath);
-
-	const modelRootPath = getModelRootPath(testPath, libraryPathName);
-
-	const outputPath = path.join(resultPath, libraryPathName + ".txt");
-
-	fse.ensureFileSync(outputPath);
-
-	const textDocument = await workspace.openTextDocument(outputPath);
-	const textEditor = await window.showTextDocument(textDocument);
-
-	let terminologyPathActual = terminologyPath;
-	if (
-		!terminologyPath ||
-		terminologyPath === "" ||
-		!fs.existsSync(terminologyPath)
-	) {
-		terminologyPathActual = "";
-	}
-
-	const cqlMessage = `CQL path: ${libraryDirectory}`;
-	const modelMessage =
-		modelRootPath && modelRootPath !== ""
-			? `Data path: ${modelRootPath}`
-			: `No data found at ${testPath}. Evaluation may fail if data is required.`;
-	const terminologyMessage =
-		terminologyPathActual && terminologyPathActual !== ""
-			? `Terminology path: ${terminologyPathActual}`
-			: `No terminology found at ${terminologyPath}. Evaluation may fail if terminology is required.`;
-
-	await insertTextAtEnd(textEditor, "Executing CQL...\r\n");
-	await insertTextAtEnd(textEditor, `${cqlMessage}\r\n`);
-	await insertTextAtEnd(textEditor, `${modelMessage}\r\n`);
-	await insertTextAtEnd(textEditor, `${terminologyMessage}\r\n`);
-
-	let operationArgs = getCqlCommandArgs(fhirVersion, optionsPath);
-
-	if (modelRootPath && modelRootPath !== "" && fs.existsSync(modelRootPath)) {
-		const dirs = fs
-			.readdirSync(modelRootPath)
-			.filter((dirent) =>
-				fs.statSync(path.join(modelRootPath, dirent)).isDirectory()
-			);
-
-		if (dirs && dirs.length > 0) {
-			dirs.forEach((dirent) => {
-				const context = dirent;
-				const modelPath = path.join(modelRootPath, dirent);
-				operationArgs = getExecArgs(
-					operationArgs,
-					libraryDirectory,
-					libraryName,
-					modelType,
-					modelPath,
-					terminologyPathActual,
-					context,
-					measurementPeriod
-				);
-			});
-		} else {
-			operationArgs = getExecArgs(
-				operationArgs,
-				libraryDirectory,
-				libraryName,
-				modelType,
-				null,
-				terminologyPathActual,
-				null,
-				measurementPeriod
-			);
-		}
-	} else {
-		operationArgs = getExecArgs(
-			operationArgs,
-			libraryDirectory,
-			libraryName,
-			modelType,
-			null,
-			terminologyPathActual,
-			null,
-			measurementPeriod
-		);
-	}
-
-	await executeCQL(textEditor, operationArgs);
+	
+	return null;
 }
 
-function getModelRootPath(parentPath: string, libraryPathName: string): string {
-	let modelRootPath = "";
-	if (fs.existsSync(parentPath)) {
-		const files = fs.readdirSync(parentPath);
-		const dirs = files.filter((dirent) =>
-			fs.statSync(path.join(parentPath, dirent)).isDirectory()
-		);
-		dirs.forEach((dirent) => {
-			if (modelRootPath === "") {
-				if (dirent === libraryPathName) {
-					modelRootPath = path.join(parentPath, dirent);
-				} else {
-					modelRootPath = getModelRootPath(
-						path.join(parentPath, dirent),
-						libraryPathName
-					);
-				}
-			}
-		});
-	}
-	return modelRootPath;
+
+interface TestCase {
+	name: string | null;
+	path: Uri | null
 }
 
-async function insertTextAtEnd(textEditor: TextEditor, text: string) {
+/**
+ * Get the test cases to execute
+ * @param testPath the root path to look for test cases
+ * @returns a list of test cases to execute
+ */
+function getTestPaths(testPath: Uri, libraryName: string): TestCase[] {
+	if (!fs.existsSync(testPath.fsPath)) {
+		return []
+	}
+
+	let testCases: TestCase[] = [];
+	let directories = glob.sync(testPath.fsPath + `/**/${libraryName}`)
+		.filter(d => fs.statSync(d).isDirectory());
+	for (var dir of directories) {
+		let cases = fs.readdirSync(dir).filter(d => fs.statSync(path.join(dir, d)).isDirectory());
+		for (var c of cases) {
+			testCases.push({ name : c, path : Uri.file(path.join(dir, c))});
+		}
+	}
+
+	return testCases
+}
+
+async function insertLineAtEnd(textEditor: TextEditor, text: string) {
 	const document = textEditor.document;
 	await textEditor.edit((editBuilder) => {
 		editBuilder.insert(
 			new Position(textEditor.document.lineCount, 0),
-			text
+			text + "\n"
 		);
 	});
 }
@@ -194,56 +163,56 @@ async function executeCQL(textEditor: TextEditor, operationArgs: string[]) {
 	);
 	const endExecution = Date.now();
 
-	await insertTextAtEnd(textEditor, result!);
-	await insertTextAtEnd(
+	await insertLineAtEnd(textEditor, result!);
+	await insertLineAtEnd(
 		textEditor,
 		`elapsed: ${(
 			(endExecution - startExecution) /
 			1000
-		).toString()} seconds\r\n\r\n`
+		).toString()} seconds`
 	);
 }
 
-function getCqlCommandArgs(fhirVersion: string, optionsPath: string): string[] {
+function getCqlCommandArgs(fhirVersion: string, optionsPath: Uri): string[] {
 	const args = ["cql"];
 
-	if (fhirVersion && fhirVersion !== "") {
-		args.push(`-fv=${fhirVersion}`);
-	} else {
-		args.push(`-fv=R4`);
-	}
+	args.push(`-fv=${fhirVersion}`);
 
-	if (optionsPath && fs.existsSync(optionsPath)) {
-		args.push(`-op=${Uri.file(optionsPath)}`);
+	if (optionsPath && fs.existsSync(optionsPath.fsPath)) {
+		args.push(`-op=${optionsPath}`);
 	}
 
 	return args;
 }
 
 function getExecArgs(
-	args: string[],
-	libraryDirectory: string,
+	libraryDirectory: Uri,
 	libraryName: string,
-	modelType: string,
-	modelPath: string | null,
-	terminologyPath: string,
+	modelPath: Uri | null,
+	terminologyPath: Uri | null,
 	contextValue: string | null,
 	measurementPeriod: string
 ): string[] {
+
+	// TODO: One day we might support other models and contexts
+	const modelType = "FHIR";
+	const contextType = "Patient";
+
+	let args : string[] = [];
 	args.push(`-ln=${libraryName}`);
-	args.push(`-lu=${Uri.file(libraryDirectory)}`);
+	args.push(`-lu=${libraryDirectory}`);
 
-	if (modelType && modelType !== "" && modelPath && modelPath !== null) {
-		args.push("-m=FHIR");
-		args.push(`-mu=${Uri.file(modelPath)}`);
+	if (modelPath) {
+		args.push(`-m=${modelType}`);
+		args.push(`-mu=${modelPath}`);
 	}
 
-	if (terminologyPath && terminologyPath !== "") {
-		args.push(`-t=${Uri.file(terminologyPath)}`);
+	if (terminologyPath) {
+		args.push(`-t=${terminologyPath}`);
 	}
 
-	if (contextValue && contextValue !== "") {
-		args.push(`-c=Patient`);
+	if (contextValue) {
+		args.push(`-c=${contextType}`);
 		args.push(`-cv=${contextValue}`);
 	}
 
