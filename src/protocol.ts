abf9b204fda100a69c2a44c6c7fa28b3b900195c
Holly Wu <holly.wu@smiledigitalhealth.com>
2024-07-18 18:40:08 -0700

    Run prettier

--- src/protocol.ts
+++ src/protocol.ts
@@ -2,78 +2,68 @@
  * This file defines the API for messages exchanged between the client and the server
  */
 
-import {
-	RequestType,
-	NotificationType,
-	ExecuteCommandParams,
-} from "vscode-languageclient";
-import { Command } from "vscode";
+import { RequestType, NotificationType, ExecuteCommandParams } from 'vscode-languageclient';
+import { Command } from 'vscode';
 
 /**
  * The message type. Copied from vscode protocol
  */
 export enum MessageType {
-	/**
-	 * An error message.
-	 */
-	Error = 1,
-	/**
-	 * A warning message.
-	 */
-	Warning = 2,
-	/**
-	 * An information message.
-	 */
-	Info = 3,
-	/**
-	 * A log message.
-	 */
-	Log = 4,
+  /**
+   * An error message.
+   */
+  Error = 1,
+  /**
+   * A warning message.
+   */
+  Warning = 2,
+  /**
+   * An information message.
+   */
+  Info = 3,
+  /**
+   * A log message.
+   */
+  Log = 4,
 }
 export interface StatusReport {
-	message: string;
-	type: string;
+  message: string;
+  type: string;
 }
 
 export interface ProgressReport {
-	id: string;
-	task: string;
-	subTask: string;
-	status: string;
-	workDone: number;
-	totalWork: number;
-	complete: boolean;
+  id: string;
+  task: string;
+  subTask: string;
+  status: string;
+  workDone: number;
+  totalWork: number;
+  complete: boolean;
 }
 
 export interface ActionableMessage {
-	severity: MessageType;
-	message: string;
-	data?: any;
-	commands?: Command[];
+  severity: MessageType;
+  message: string;
+  data?: any;
+  commands?: Command[];
 }
 export namespace StatusNotification {
-	export const type = new NotificationType<StatusReport>("language/status");
+  export const type = new NotificationType<StatusReport>('language/status');
 }
 
 export namespace ProgressReportNotification {
-	export const type = new NotificationType<ProgressReport>(
-		"language/progressReport"
-	);
+  export const type = new NotificationType<ProgressReport>('language/progressReport');
 }
 export namespace ActionableNotification {
-	export const type = new NotificationType<ActionableMessage>(
-		"language/actionableNotification"
-	);
+  export const type = new NotificationType<ActionableMessage>('language/actionableNotification');
 }
 
 export namespace ExecuteClientCommandRequest {
-	export const type = new RequestType<ExecuteCommandParams, any, void>(
-		"workspace/executeClientCommand"
-	);
+  export const type = new RequestType<ExecuteCommandParams, any, void>(
+    'workspace/executeClientCommand',
+  );
 }
 
 export namespace ServerNotification {
-	export const type = new NotificationType<ExecuteCommandParams>(
-		"workspace/notify"
-	);
+  export const type = new NotificationType<ExecuteCommandParams>('workspace/notify');
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,30 +1,30 @@
-import * as fs from "fs";
-import * as path from "path";
+import * as fs from 'fs';
+import * as path from 'path';
 
 export function deleteDirectory(dir: fs.PathLike) {
-	if (fs.existsSync(dir)) {
-		fs.readdirSync(dir).forEach((child) => {
-			const entry = path.join(dir.toString(), child);
-			if (fs.lstatSync(entry).isDirectory()) {
-				deleteDirectory(entry);
-			} else {
-				fs.unlinkSync(entry);
-			}
-		});
-		fs.rmdirSync(dir);
-	}
+  if (fs.existsSync(dir)) {
+    fs.readdirSync(dir).forEach(child => {
+      const entry = path.join(dir.toString(), child);
+      if (fs.lstatSync(entry).isDirectory()) {
+        deleteDirectory(entry);
+      } else {
+        fs.unlinkSync(entry);
+      }
+    });
+    fs.rmdirSync(dir);
+  }
 }
 
 export function getTimestamp(file: fs.PathLike) {
-	if (!fs.existsSync(file)) {
-		return -1;
-	}
-	const stat = fs.statSync(file);
-	return stat.mtimeMs;
+  if (!fs.existsSync(file)) {
+    return -1;
+  }
+  const stat = fs.statSync(file);
+  return stat.mtimeMs;
 }
 
 export function ensureExists(folder: fs.PathLike) {
-	if (!fs.existsSync(folder)) {
-		fs.mkdirSync(folder);
-	}
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+  }
 }
