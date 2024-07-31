5e5ba2ae171c25a03cde96a547229dc15423b43c
Holly Wu <holly.wu@smiledigitalhealth.com>
2024-07-31 13:43:29 -0700

    Fix usages of destructured values

--- src/executeCql.ts
+++ src/executeCql.ts
@@ -11,44 +11,44 @@ async function insertLineAtEnd(textEditor: TextEditor, text: string) {
   });
 }
 
-export async function executeCQL({operationArgs, testPath, outputPath}: EvaluationParameters) {
+export async function executeCQL({ operationArgs, testPath, outputPath }: EvaluationParameters) {
   let cqlMessage = '';
   let terminologyMessage = '';
   let testMessage = `Test cases:\n`;
   let foundTest = false;
-  for (let i = 0; i < evaluationParameters.operationArgs?.length!; i++) {
-    if (evaluationParameters.operationArgs![i].startsWith("-lu=")) {
-      cqlMessage = `CQL: ${evaluationParameters.operationArgs![i].substring(4)}`;
-    } else if (evaluationParameters.operationArgs![i].startsWith("-t=")) {
-      let terminologyUri = Uri.parse(evaluationParameters.operationArgs![i].substring(3));
+  for (let i = 0; i < operationArgs?.length!; i++) {
+    if (operationArgs![i].startsWith('-lu=')) {
+      cqlMessage = `CQL: ${operationArgs![i].substring(4)}`;
+    } else if (operationArgs![i].startsWith('-t=')) {
+      let terminologyUri = Uri.parse(operationArgs![i].substring(3));
       terminologyMessage = fs.existsSync(terminologyUri.fsPath)
-      ? `Terminology: ${evaluationParameters.operationArgs![i].substring(3)}`
-      : `No terminology found at ${evaluationParameters.operationArgs![i].substring(3)}. Evaluation may fail if terminology is required.`;
-    } else if (evaluationParameters.operationArgs![i].startsWith("-mu=")) {
-        foundTest = true;
-        testMessage += `${evaluationParameters.operationArgs![i].substring(4)}`;
-    } else if (evaluationParameters.operationArgs![i].startsWith("-cv=")) {
-        foundTest = true;
-        testMessage += ` - ${evaluationParameters.operationArgs![i].substring(4)} \n` 
+        ? `Terminology: ${operationArgs![i].substring(3)}`
+        : `No terminology found at ${operationArgs![i].substring(3)}. Evaluation may fail if terminology is required.`;
+    } else if (operationArgs![i].startsWith('-mu=')) {
+      foundTest = true;
+      testMessage += `${operationArgs![i].substring(4)}`;
+    } else if (operationArgs![i].startsWith('-cv=')) {
+      foundTest = true;
+      testMessage += ` - ${operationArgs![i].substring(4)} \n`;
     }
   }
 
   if (!foundTest) {
-    testMessage = `No data found at path ${evaluationParameters.testPath!}. Evaluation may fail if data is required.`;
+    testMessage = `No data found at path ${testPath!}. Evaluation may fail if data is required.`;
   }
 
-  const textDocument = await workspace.openTextDocument(evaluationParameters.outputPath!);
+  const textDocument = await workspace.openTextDocument(outputPath!);
   const textEditor = await window.showTextDocument(textDocument);
-  
+
   await insertLineAtEnd(textEditor, `${cqlMessage}`);
   await insertLineAtEnd(textEditor, `${terminologyMessage}`);
   await insertLineAtEnd(textEditor, `${testMessage}`);
-  
+
   const startExecution = Date.now();
   const result: string | undefined = await commands.executeCommand(
     Commands.EXECUTE_WORKSPACE_COMMAND,
     Commands.EXECUTE_CQL,
-    ...evaluationParameters.operationArgs!,
+    ...operationArgs!,
   );
   const endExecution = Date.now();
 
