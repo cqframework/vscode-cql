eface7d8f519623a16e9a7e4d578140c144aa5d3
Joshua Reynolds <jreysteven1@gmail.com>
2024-08-14 09:33:45 -0600

    Add in the local connection check for default behaviour

--- src/buildParameters.ts
+++ src/buildParameters.ts
@@ -1,9 +1,11 @@
+import { glob } from 'glob';
 import { Uri, window, workspace } from 'vscode';
 import { Utils } from 'vscode-uri';
 
 import * as fs from 'fs';
 import * as fse from 'fs-extra';
-import { ConnectionManager } from './connectionManager';
+import path from 'path';
+import { Connection, ConnectionManager, Context } from './connectionManager';
 
 export type EvaluationParameters = {
   operationArgs: string[] | undefined;
@@ -27,10 +29,12 @@ export function buildParameters(uri: Uri, expression: string | undefined): Evalu
   const testPath = Utils.resolvePath(projectPath, 'input', 'tests');
   const resultPath = Utils.resolvePath(testPath, 'results');
   const outputPath = Utils.resolvePath(resultPath, `${libraryName}.txt`);
-  const connectionManager = new ConnectionManager();
+  const connectionManager = ConnectionManager.getManager();
 
   fse.ensureFileSync(outputPath.fsPath);
 
+  let contexts = connectionManager.getCurrentContexts();
+  let connection = connectionManager.getCurrentConnection();
   let operationArgs = getCqlCommandArgs(
     fhirVersion,
     optionsPath,
@@ -38,7 +42,12 @@ export function buildParameters(uri: Uri, expression: string | undefined): Evalu
     libraryName,
     expression,
     terminologyPath,
-    connectionManager,
+    connection,
+    // I kind of want to make 'Local' a const that I can share....
+    // not sure, but I already ran into an issue debugging when I changed the check below, but not this one
+    contexts != undefined && connection?.name !== 'Local' && Object.values(contexts).length > 0
+      ? new Map(Object.values(contexts).map(context => ['', context]))
+      : getLocalContexts(testPath, libraryName),
     measurementPeriod,
   );
 
@@ -57,7 +66,8 @@ function getCqlCommandArgs(
   libraryName: string,
   expression: string | undefined,
   terminologyPath: Uri,
-  connectionManager: ConnectionManager,
+  connection: Connection | undefined,
+  contexts: Map<string, Context>,
   measurementPeriod: string,
 ): string[] {
   const args = ['cql'];
@@ -66,14 +76,11 @@ function getCqlCommandArgs(
   if (optionsPath && fs.existsSync(optionsPath.fsPath)) {
     args.push(`-op=${optionsPath}`);
   }
-
-  let connection = connectionManager.getCurrentConnection();
-  let modelPath: string | undefined = connection?.endpoint;
-  let contexts = connectionManager.getCurrentContexts();
   const modelType = 'FHIR';
+  let modelPath: string | undefined = connection?.endpoint;
 
   if (contexts) {
-    Object.entries(contexts).forEach(([key, value]) => {
+    contexts.forEach((value: Context, key: string) => {
       args.push(`-ln=${libraryName}`);
       args.push(`-lu=${libraryDirectory}`);
 
@@ -85,6 +92,10 @@ function getCqlCommandArgs(
         args.push(`-t=${terminologyPath}`);
       }
 
+      if (connection?.name === 'Local') {
+        // connection.endpoint = projectPath.toString(); Can't use projectPath because Evaluator is not ok with cql-option.json and other files that are not fhir resources.
+        modelPath = key;
+      }
       if (modelPath) {
         args.push(`-m=${modelType}`);
         args.push(`-mu=${modelPath}`);
@@ -100,6 +111,28 @@ function getCqlCommandArgs(
   }
 
   return args;
+}
+
+function getLocalContexts(testPath: Uri, libraryName: string): Map<string, Context> {
+  let testCases: Map<string, Context> = new Map<string, Context>();
+  if (!fs.existsSync(testPath.fsPath)) {
+    return testCases;
+  }
+  let directories = glob
+    .sync(testPath.fsPath + `/**/${libraryName}`)
+    .filter(d => fs.statSync(d).isDirectory());
+  for (var dir of directories) {
+    let cases = fs.readdirSync(dir).filter(d => fs.statSync(path.join(dir, d)).isDirectory());
+    for (var c of cases) {
+      // Should really be reading in Patient Resources and getting the ids and everything should be based on a repository like in the evaluator
+      testCases.set(Uri.file(path.join(dir, c)).toString(), {
+        resourceType: 'Patient',
+        resourceID: c,
+      });
+      // path: Uri.file(path.join(dir, c))  For Patient specific directory
+    }
+  }
+  return testCases;
 }
 
 function getFhirVersion(): string {
