ba75e9a26b48af86ba0d7a6262655fc381e3c78e
Joshua Reynolds <jreysteven1@gmail.com>
2024-08-15 13:00:34 -0600

    Initial Jest setup

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,20 +1,26 @@
-import * as assert from 'assert';
+// import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+// // You can import and use all API from the 'vscode' module
+// // as well as import your extension to test it
+// import * as vscode from 'vscode';
+// // import * as myExtension from '../../extension';
 
-suite('Extension Activation Test', () => {
-  test('Extension should be present', () => {
-    assert.ok(vscode.extensions.getExtension('cqframework.cql'));
-  });
+// suite('Extension Activation Test', () => {
+//   test('Extension should be present', () => {
+//     assert.ok(vscode.extensions.getExtension('cqframework.cql'));
+//   });
 
-  test('Extension should be active', async () => {
-    const extension = vscode.extensions.getExtension('cqframework.cql');
-    if (!extension?.isActive) {
-      await extension?.activate();
-    }
-    assert.ok(extension && extension.isActive);
+//   test('Extension should be active', async () => {
+//     const extension = vscode.extensions.getExtension('cqframework.cql');
+//     if (!extension?.isActive) {
+//       await extension?.activate();
+//     }
+//     assert.ok(extension && extension.isActive);
+//   });
+// });
+
+describe('Extension Test Place Holder', () => {
+  test('TBD move extension test logic to jest format.', () => {
+    console.log('Test hit');
   });
 });
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,38 +1,44 @@
-import * as path from 'path';
-import Mocha from 'mocha';
-import glob from 'glob';
+// import * as path from 'path';
+// import Mocha from 'mocha';
+// import glob from 'glob';
 
-export function run(): Promise<void> {
-  // Create the mocha test
-  const mocha = new Mocha({
-    ui: 'tdd',
-    timeout: 50000,
+// export function run(): Promise<void> {
+//   // Create the mocha test
+//   const mocha = new Mocha({
+//     ui: 'tdd',
+//     timeout: 50000,
+//   });
+
+//   const testsRoot = path.resolve(__dirname, '..');
+
+//   return new Promise((c, e) => {
+//     glob('**/**.test.js', { cwd: testsRoot }, (err: any, files: any[]) => {
+//       if (err) {
+//         return e(err);
+//       }
+
+//       // Add files to the test suite
+//       files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
+
+//       try {
+//         // Run the mocha test
+//         mocha.run(failures => {
+//           if (failures > 0) {
+//             e(new Error(`${failures} tests failed.`));
+//           } else {
+//             c();
+//           }
+//         });
+//       } catch (err) {
+//         console.error(err);
+//         e(err);
+//       }
+//     });
+//   });
+// }
+
+describe('Index Test Place Holder', () => {
+  test('TBD move index test logic to jest format.', () => {
+    console.log('Test hit');
   });
-
-  const testsRoot = path.resolve(__dirname, '..');
-
-  return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err: any, files: any[]) => {
-      if (err) {
-        return e(err);
-      }
-
-      // Add files to the test suite
-      files.forEach((f: string) => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.run(failures => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (err) {
-        console.error(err);
-        e(err);
-      }
-    });
-  });
-}
+});
