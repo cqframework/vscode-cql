9b71e84bcba77f90d666a89782a3d78c66385acc
Joshua Reynolds <jreysteven1@gmail.com>
2024-08-15 13:00:34 -0600

    Initial Jest setup

--- src/test/suite/connectionManager.test.ts
+++ src/test/suite/connectionManager.test.ts
@@ -70,3 +70,9 @@ export function connectionManagerTester(manager: ConnectionManager): void {
 
   console.log('End of Connection Manager Testing');
 }
+
+describe('Connection Manager Test Place Holder', () => {
+  test('TBD add connection manager test logic to jest format.', () => {
+    console.log('Test hit');
+  });
+});
