1da212227e05b598fcea3dff508153daa2b442d7
holly-smile <166412459+holly-smile@users.noreply.github.com>
2024-08-15 14:31:55 -0700

    Jest Setup (#57)
    * Initial Jest setup
    
    * Update config
    
    ---------
    
    Co-authored-by: Joshua Reynolds <jreysteven1@gmail.com>

---
+++ src/test/mocks/vscode.mock.ts
@@ -0,0 +1,64 @@
+import { Uri as vscodeUri, WorkspaceConfiguration, WorkspaceFolder } from 'vscode';
+
+export const Uri = {
+  file: jest.fn((path: string): vscodeUri => {
+    return {
+      fsPath: path,
+    } as vscodeUri;
+  }),
+  parse: jest.fn((path: string): vscodeUri => {
+    return {
+      fsPath: path,
+    } as vscodeUri;
+  }),
+};
+
+export const window = {
+  createOutputChannel: jest.fn(),
+  showQuickPick: jest.fn(),
+  showInformationMessage: jest.fn(),
+  showErrorMessage: jest.fn(),
+  createTerminal: jest.fn(),
+  activeTextEditor: {
+    document: {
+      getText: jest.fn(),
+    },
+  },
+};
+
+export const workspace = {
+  getWorkspaceFolder: jest.fn((uri: vscodeUri): WorkspaceFolder | undefined => {
+    return {
+      uri: Uri.file('./src/vscode-cql/src/test/suite/resources/simple-test-ig'),
+      name: 'simple-test-ig',
+      index: 0,
+    } as WorkspaceFolder;
+  }),
+  getConfiguration: jest.fn((): WorkspaceConfiguration => {
+    return {
+      get: jest.fn(),
+      has: jest.fn(),
+      inspect: jest.fn(),
+      update: jest.fn(),
+    } as unknown as WorkspaceConfiguration;
+  }),
+};
+
+export const env = {
+  clipboard: {
+    readText: jest.fn(),
+    writeText: jest.fn(),
+  },
+};
+
+export const commands = {
+  executeCommand: jest.fn(),
+};
+
+export default {
+  Uri,
+  window,
+  workspace,
+  env,
+  commands,
+};
