f1526ed7fe34d10e169e273e2cecbbf88d2e550e
Joshua Reynolds <jreysteven1@gmail.com>
2024-08-15 13:03:38 -0600

    Added buildParameters.test.ts

--- src/buildParameters.ts
+++ src/buildParameters.ts
@@ -1,11 +1,11 @@
-import { glob } from "glob";
-import { window, workspace } from "vscode";
-import { URI, Utils } from "vscode-URI";
+import { glob } from 'glob';
+import { window, workspace } from 'vscode';
+import { URI, Utils } from 'vscode-uri';
 
-import * as fs from "fs";
-import * as fse from "fs-extra";
-import path from "path";
-import { Connection, ConnectionManager, Context } from "./connectionManager";
+import * as fs from 'fs';
+import * as fse from 'fs-extra';
+import path from 'path';
+import { Connection, ConnectionManager, Context } from './connectionManager';
 
 export type EvaluationParameters = {
   operationArgs: string[] | undefined;
@@ -14,14 +14,9 @@ export type EvaluationParameters = {
 };
 
 // Should be working with normalized data
-export function buildParameters(
-  uri: URI,
-  expression: string | undefined
-): EvaluationParameters {
+export function buildParameters(uri: URI, expression: string | undefined): EvaluationParameters {
   if (!fs.existsSync(uri.fsPath)) {
-    window.showInformationMessage(
-      "No library content found. Please save before executing."
-    );
+    window.showInformationMessage('No library content found. Please save before executing.');
     return {
       operationArgs: undefined,
       outputPath: undefined,
@@ -30,19 +25,14 @@ export function buildParameters(
   }
 
   const libraryDirectory = Utils.dirname(uri);
-  const libraryName = Utils.basename(uri).replace(".cql", "").split("-")[0];
+  const libraryName = Utils.basename(uri).replace('.cql', '').split('-')[0];
   const projectPath = workspace.getWorkspaceFolder(uri)!.uri;
-  const terminologyPath: URI = Utils.resolvePath(
-    projectPath,
-    "input",
-    "vocabulary",
-    "valueset"
-  );
+  const terminologyPath: URI = Utils.resolvePath(projectPath, 'input', 'vocabulary', 'valueset');
   const fhirVersion = getFhirVersion();
-  const optionsPath = Utils.resolvePath(libraryDirectory, "cql-options.json");
-  const measurementPeriod = "";
-  const testPath = Utils.resolvePath(projectPath, "input", "tests");
-  const resultPath = Utils.resolvePath(testPath, "results");
+  const optionsPath = Utils.resolvePath(libraryDirectory, 'cql-options.json');
+  const measurementPeriod = '';
+  const testPath = Utils.resolvePath(projectPath, 'input', 'tests');
+  const resultPath = Utils.resolvePath(testPath, 'results');
   const outputPath = Utils.resolvePath(resultPath, `${libraryName}.txt`);
   const connectionManager = ConnectionManager.getManager();
 
@@ -53,13 +43,9 @@ export function buildParameters(
 
   if (
     contexts === undefined ||
-    (contexts != undefined &&
-      connection?.name !== "Local" &&
-      Object.values(contexts).length === 0)
+    (contexts != undefined && connection?.name !== 'Local' && Object.values(contexts).length === 0)
   ) {
-    window.showErrorMessage(
-      "Remote connection is selected but no contexts are provided."
-    );
+    window.showErrorMessage('Remote connection is selected but no contexts are provided.');
   }
 
   let operationArgs = getCqlCommandArgs({
@@ -73,12 +59,8 @@ export function buildParameters(
     // I kind of want to make 'Local' a const that I can share....
     // not sure, but I already ran into an issue debugging when I changed the check below, but not this one
     contexts:
-      contexts != undefined &&
-      connection?.name !== "Local" &&
-      Object.values(contexts).length > 0
-        ? new Map(
-            Object.entries(contexts).map(([key, context]) => [key, context])
-          )
+      contexts != undefined && connection?.name !== 'Local' && Object.values(contexts).length > 0
+        ? new Map(Object.entries(contexts).map(([key, context]) => [key, context]))
         : getLocalContexts(testPath, libraryName),
     measurementPeriod,
   });
@@ -112,13 +94,13 @@ function getCqlCommandArgs({
   contexts: Map<string, Context>;
   measurementPeriod: string;
 }): string[] {
-  const args = ["cql"];
+  const args = ['cql'];
 
   args.push(`-fv=${fhirVersion}`);
   if (optionsPath && fs.existsSync(optionsPath.fsPath)) {
     args.push(`-op=${optionsPath}`);
   }
-  const modelType = "FHIR";
+  const modelType = 'FHIR';
   let modelPath: string | undefined = connection?.endpoint;
 
   if (contexts) {
@@ -134,7 +116,7 @@ function getCqlCommandArgs({
         args.push(`-t=${terminologyPath}`);
       }
 
-      if (connection?.name === "Local") {
+      if (connection?.name === 'Local') {
         // connection.endpoint = projectPath.toString(); Can't use projectPath because Evaluator is not ok with cql-option.json and other files that are not fhir resources.
         modelPath = key;
       }
@@ -143,7 +125,7 @@ function getCqlCommandArgs({
         args.push(`-mu=${modelPath}`);
       }
 
-      if (measurementPeriod && measurementPeriod !== "") {
+      if (measurementPeriod && measurementPeriod !== '') {
         args.push(`-p=${libraryName}."Measurement Period"`);
         args.push(`-pv=${measurementPeriod}`);
       }
@@ -155,25 +137,20 @@ function getCqlCommandArgs({
   return args;
 }
 
-function getLocalContexts(
-  testPath: URI,
-  libraryName: string
-): Map<string, Context> {
+function getLocalContexts(testPath: URI, libraryName: string): Map<string, Context> {
   let testCases: Map<string, Context> = new Map<string, Context>();
   if (!fs.existsSync(testPath.fsPath)) {
     return testCases;
   }
   let directories = glob
     .sync(testPath.fsPath + `/**/${libraryName}`)
-    .filter((d) => fs.statSync(d).isDirectory());
+    .filter(d => fs.statSync(d).isDirectory());
   for (let dir of directories) {
-    let cases = fs
-      .readdirSync(dir)
-      .filter((d) => fs.statSync(path.join(dir, d)).isDirectory());
+    let cases = fs.readdirSync(dir).filter(d => fs.statSync(path.join(dir, d)).isDirectory());
     for (let c of cases) {
       // Should really be reading in Patient Resources and getting the ids and everything should be based on a repository like in the evaluator
       testCases.set(URI.file(path.join(dir, c)).toString(), {
-        resourceType: "Patient",
+        resourceType: 'Patient',
         resourceID: c,
       });
       // path: URI.file(path.join(dir, c))  For Patient specific directory
@@ -184,23 +161,19 @@ function getLocalContexts(
 
 function getFhirVersion(): string {
   const fhirVersionRegex = /using (FHIR|"FHIR") version '(\d(.|\d)*)'/;
-  const matches = window
-    .activeTextEditor!.document.getText()
-    .match(fhirVersionRegex);
+  const matches = window.activeTextEditor!.document.getText().match(fhirVersionRegex);
   if (matches && matches.length > 2) {
     const version = matches[2];
-    if (version.startsWith("2")) {
-      return "DSTU2";
-    } else if (version.startsWith("3")) {
-      return "DSTU3";
-    } else if (version.startsWith("4")) {
-      return "R4";
-    } else if (version.startsWith("5")) {
-      return "R5";
+    if (version.startsWith('2')) {
+      return 'DSTU2';
+    } else if (version.startsWith('3')) {
+      return 'DSTU3';
+    } else if (version.startsWith('4')) {
+      return 'R4';
+    } else if (version.startsWith('5')) {
+      return 'R5';
     }
   }
-  window.showInformationMessage(
-    "Unable to determine version of FHIR used. Defaulting to R4."
-  );
-  return "R4";
+  window.showInformationMessage('Unable to determine version of FHIR used. Defaulting to R4.');
+  return 'R4';
 }
